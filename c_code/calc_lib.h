84df5dcd275196f46d26ae767ad65e956bdcf775
osan <osannnn@gmail.com>
2023-11-08 11:54:12 +0100

    created folder for the libs and source in C. Added 2 libraries

---
+++ c_code/calc_lib.h
@@ -0,0 +1,10 @@
+//signs for the calculator
+#ifndef CALC_LIB_H
+#define CALC_LIB_H
+
+
+
+
+
+
+#endif
---
+++ c_code/user_data.h
@@ -0,0 +1,9 @@
+//lib for the sign of user data insertion (database?)
+#ifndef USER_DATA_H
+#define USER_DATA_H
+
+
+
+
+
+#endif
